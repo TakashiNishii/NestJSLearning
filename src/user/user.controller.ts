7e61ed7c3fa3128f2de20b96f0755dad8f687e43
TakashiNishii <takashi@geeklys.com>
2024-05-11 00:37:42 -0400

    :rocket: Update user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -9,6 +9,8 @@ import {
   Put,
 } from '@nestjs/common';
 import { CreateUserDTO } from './dto/create-user.dto';
+import { UpdatePutUserDTO } from './dto/update-put-user.dto';
+import { UpdatePatchUserDTO } from './dto/update-patch-user.dto';
 
 @Controller('users')
 export class UserController {
@@ -28,19 +30,29 @@ export class UserController {
   }
 
   @Put(':id')
-  async update(@Body() body, @Param() params) {
+  async update(
+    @Body() { email, name, password }: UpdatePutUserDTO,
+    @Param() params,
+  ) {
     return {
       method: 'PUT',
-      body,
+      email,
+      name,
+      password,
       params,
     };
   }
 
   @Patch(':id')
-  async updatePartial(@Body() body, @Param() params) {
+  async updatePartial(
+    @Body() { email, name, password }: UpdatePatchUserDTO,
+    @Param() params,
+  ) {
     return {
       method: 'PATCH',
-      body,
+      email,
+      name,
+      password,
       params,
     };
   }
