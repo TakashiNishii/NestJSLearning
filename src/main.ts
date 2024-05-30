0da6e98bc56a8c616dca06ca4e55333d7e3c9add
TakashiNishii <takashi@geeklys.com>
2024-05-30 14:21:32 -0400

    :wrench: Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,12 @@ import { LogInterceptor } from './interceptors/log.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.enableShutdownHooks();
   app.useGlobalInterceptors(new LogInterceptor()); //Aqui aplica o interceptor globalmente
+
   await app.listen(3000);
 }
 bootstrap();
